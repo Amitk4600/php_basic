e1f9bbefaf262ef0888c24bb9cce3aac56101a02
Amitk4600 <amitk4600@gmail.com>
2024-06-04 16:38:42 +0530

    display total active and inactive level wise

--- Level_member.php
+++ Level_member.php
@@ -3,12 +3,15 @@ session_start();
 include "./registration/connection.php";
 
 $id = $_SESSION['id'];
-
+if (!isset($_SESSION['id'])) {
+    header("location: http://localhost/amit_php/login/index.php");
+    exit();
+}
 // count all level member
 
 function levelMemberFunction($conn, $referral, $a, $level)
 {
-    $x = "SELECT * FROM `registration` WHERE `joining_referral` = '" . $referral . "'";
+    $x = "SELECT * FROM `registration` WHERE `joining_referral` = '" . $referral . "' ";
     $y = mysqli_query($conn, $x);
     $n = mysqli_num_rows($y);
     if ($n > 0) {
@@ -25,17 +28,17 @@ function levelMemberFunction($conn, $referral, $a, $level)
 
 $a = [];
 $uuuid = $id; //userid
-
 $qury = "SELECT * FROM `registration` WHERE `user_id` = '" . $uuuid . "'";
-
 $result = mysqli_query($conn, $qury);
 $num = mysqli_num_rows($result);
 if ($num > 0) {
     while ($fetch = mysqli_fetch_assoc($result)) {
-        $level = 1;
+    
+        $level = 0;
         $fetch['level'] = $level;
         $aa = array_push($a, $fetch);
         $a = levelMemberFunction($conn, $fetch['referral'], $a, $level);
+      
     }
 }
 
@@ -50,118 +53,100 @@ echo "<h4> Total team $count</h4>";
 echo "<br>";
 $totalActiveMembers = 0;
 $totalInactiveMembers = 0;
-
+echo "<table class='table table-striped'>";
+echo "  <tr>";
+echo "    <th scope='col'></th>";
+echo "    <th scope='col'>ID</th>";
+echo "    <th scope='col'>Name</th>";
+echo "    <th scope='col'>Active_Inactive Member</th>";
+echo "    <th scope='col'>Levels</th>";
+echo "  </tr>";
+$count = 1;
 foreach ($a as $member) {
-    if ($member['active_and_inactive'] == 'active') {
+    echo "  <tr>";
+    echo "    <td>" . $count++ . "</td>";
+    echo "    <td>" . $member['user_id'] . "</td>";
+    echo "    <td>" . $member['names'] . "</td>";
+
+    // Add status based on active_and_inactive value
+    if ($member['active_and_inactive'] === 'active') {
         $totalActiveMembers++;
-    } elseif ($member['active_and_inactive'] == 'inactive') {
+        echo "    <td style='background: green; color:white; width: 80px; text-align:center'>Active</td>"; 
+    } else {
         $totalInactiveMembers++;
+        echo "    <td style='background: red; color:white; width: 80px; text-align:center'>Inactive</td>";
     }
+    echo "    <td style='text-align:center;width: 80px;'>" . $member['level'] . "</td>";
+    echo "  </tr>";
+}
+echo "</table>";
+
+// (Optional) Print summary of active and inactive members
+echo "<p>Total Active Members: $totalActiveMembers</p>";
+echo "<p>Total Inactive Members: $totalInactiveMembers</p>";
+
+$levelCounts = [];
+
+function countMembersByLevel($member) {
+    global $levelCounts;
+    $level = $member['level'];
+    if (!isset($levelCounts[$level])) {
+        $levelCounts[$level] = [
+            'active' => 0,
+            'inactive' => 0,
+            'total' => 0,
+        ];
+    }
+
+    if ($member['active_and_inactive'] === 'active') {
+        $levelCounts[$level]['active']++;
+    } else {
+        $levelCounts[$level]['inactive']++;
+    }
+    $levelCounts[$level]['total']++;
 }
 
-echo " active members: " . $totalActiveMembers . "<br>";
-echo " inactive members: " . $totalInactiveMembers . "<br>";
+// Count members by level
+array_walk($a, 'countMembersByLevel');
 
+// Display level-wise data
+echo "<h4> Team Members by Level</h4>";
+echo "<table class='table table-striped'>";
+echo "  <tr>";
+echo "    <th scope='col'>Level</th>";
+echo "    <th scope='col'>Active</th>";
+echo "    <th scope='col'>Inactive</th>";
+echo "    <th scope='col'>Total</th>";
+echo "  </tr>";
 
-$level = isset($_GET['level']) ? $_GET['level'] : 1;  // Default level to 1 if not set
-
-$query = "SELECT * FROM Registration WHERE `user_id` = '$id'";
-$result = mysqli_query($conn, $query);
-$num = mysqli_num_rows($result);
-$row = mysqli_fetch_assoc($result);
-
-$levels = [
-    1 => $row['level1'],
-    2 => $row['level2'],
-    3 => $row['level3'],
-    4 => $row['level4'],
-    5 => $row['level5']
-];
-
-$levelData = isset($levels[$level]) ? $levels[$level] : "";
-
-$userIds = explode(" ", $levelData);
-
-$status_filter = "";
-if (isset($_GET['status'])) {
-    $status = $_GET['status'];
-    if ($status == "active") {
-        $status_filter = " AND `active_and_inactive` = 'active'";
-    } elseif ($status == "inactive") {
-        $status_filter = " AND `active_and_inactive` = 'inactive'";
-    }
+// Loop through level counts and display data
+foreach ($levelCounts as $level => $counts) {
+    echo "  <tr>";
+    echo "    <td>" . $level . "</td>";
+    echo "    <td>" . $counts['active'] . "</td>";
+    echo "    <td>" . $counts['inactive'] . "</td>";
+    echo "    <td>" . $counts['total'] . "</td>";
+    echo "  </tr>";
 }
 
-// Fetch user details for the specified level
-$query = "SELECT * FROM registration WHERE user_id IN ('" . implode("','", $userIds) . "')" . $status_filter;
-$result = mysqli_query($conn, $query);
-
-// Count active users
-$query_active_count = "SELECT COUNT(*) as active_count FROM registration WHERE user_id IN ('" . implode("','", $userIds) . "') AND `active_and_inactive` = 'active'";
-$result_active_count = mysqli_query($conn, $query_active_count);
-$row_active_count = mysqli_fetch_assoc($result_active_count);
-$active_count = $row_active_count['active_count'];
-
-// Count inactive users
-$query_inactive_count = "SELECT COUNT(*) as inactive_count FROM registration WHERE user_id IN ('" . implode("','", $userIds) . "') AND `active_and_inactive` = 'inactive'";
-$result_inactive_count = mysqli_query($conn, $query_inactive_count);
-$row_inactive_count = mysqli_fetch_assoc($result_inactive_count);
-$inactive_count = $row_inactive_count['inactive_count'];
-$total_count = $active_count + $inactive_count;
+echo "</table>";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+
     <title>Document</title>
 </head>
+
 <body>
-    <div class="container">
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th scope="col">Level</th>
-                    <th scope="col">Active</th>
-                    <th scope="col">Inactive</th>
-                    <th scope="col">Total</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($levels as $lvl => $data) {
-                    $userIds = explode(" ", $data);
-                    
-                    // Count active users for this level
-                    $query_active_count = "SELECT COUNT(*) as active_count FROM registration WHERE user_id IN ('" . implode("','", $userIds) . "') AND `active_and_inactive` = 'active'";
-                    $result_active_count = mysqli_query($conn, $query_active_count);
-                    $row_active_count = mysqli_fetch_assoc($result_active_count);
-                    $active_count = $row_active_count['active_count'];
-                    
-                    // Count inactive users for this level
-                    $query_inactive_count = "SELECT COUNT(*) as inactive_count FROM registration WHERE user_id IN ('" . implode("','", $userIds) . "') AND `active_and_inactive` = 'inactive'";
-                    $result_inactive_count = mysqli_query($conn, $query_inactive_count);
-                    $row_inactive_count = mysqli_fetch_assoc($result_inactive_count);
-                    $inactive_count = $row_inactive_count['inactive_count'];
-                    $total_count = $active_count + $inactive_count;
-                ?>
-                <tr>
-                    <td>Level <?php echo $lvl; ?></td>
-                    <td><?php echo $active_count; ?></td>
-                    <td><?php echo $inactive_count; ?></td>
-                    <td><?php echo $total_count; ?></td>
-                </tr>
-                <?php } ?>
-            </tbody>
-            
-            
-        </table>
-    </div>
-
-    
+     
 </body>
+
 </html>
-
-
